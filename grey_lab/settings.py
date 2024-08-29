cc6ea71f10478a92bb8fc0914f793aeb58bb10ab
MerinaMou110 <122225168+MerinaMou110@users.noreply.github.com>
2024-08-29 17:06:03 +0600

    update

--- grey_lab/settings.py
+++ grey_lab/settings.py
@@ -25,8 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--c^zjp*^a!g&f1ou$+-cy@m&nfwc#^#)f=vn=2mmv#$+vci@jm"
 
+# SECRET_KEY = "django-insecure--c^zjp*^a!g&f1ou$+-cy@m&nfwc#^#)f=vn=2mmv#$+vci@jm"
+SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
